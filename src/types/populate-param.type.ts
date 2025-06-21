63d1769ce83ab297ed71d2548b05faa4ea39f046
Svetlozar Argirov <zarrro@gmail.com>
2025-06-21 11:53:28 +0300

    Adjust the type of expand parameters

--- src/types/populate-param.type.ts
+++ src/types/populate-param.type.ts
@@ -1,28 +1,31 @@
 import { Collection, Populate } from '@mikro-orm/core';
 
-export type RelationKeysWithDot<Entity, Depth extends number = 3> = Depth extends 0
-  ? never
-  : {
-      [K in keyof Entity]: K extends string
-        ? Entity[K] extends (...args: any[]) => any
+export type Primitive =
+  | string
+  | number
+  | boolean
+  | bigint
+  | symbol
+  | undefined
+  | null;
+
+type SimpleNestedPaths<T, D extends 0 | 1 | 2 | 3 = 3> = 
+  D extends 0
+    ? never
+    : {
+        [K in keyof T]-?: T[K] extends Primitive
           ? never
-          : Entity[K] extends any[] 
-            ? Entity[K][number] extends { id: any }
-              ? `${K}.` | `${K}.${RelationKeysWithDot<Entity[K][number], Prev<Depth>>}`
-              : never
-            : Entity[K] extends Collection<infer U>
-              ? U extends { id: any }
-                ? `${K}.` | (RelationKeysWithDot<U, Prev<Depth>> extends infer R extends string? R extends never ? never : `${K}.${R}` : never)
-                : never
-              : Entity[K] extends { id: any }
-                ? Entity[K] extends Date | string | number | boolean 
-                  ? never 
-                  : `${K}.` | (RelationKeysWithDot<Entity[K], Prev<Depth>> extends infer R extends string ? R extends never ? never : `${K}.${R}` : never)
-                : never
-        : never;
-    }[keyof Entity];
-
-type Prev<T extends number> = T extends 0 ? never : T extends 1 ? 0 : T extends 2 ? 1 : T extends 3 ? 2 : T extends 4 ? 3 : never;
-
-
-export type PopulateParameters<Entity> = Populate<Entity, RelationKeysWithDot<Entity>>
+          : T[K] extends any[]
+          ? K extends string ? K : never
+          : T[K] extends Collection<infer U>
+          ? K extends string 
+            ? K | `${K}.${SimpleNestedPaths<U, D extends 3 ? 2 : D extends 2 ? 1 : 0> & string}`
+            : never
+          : T[K] extends Record<string, any>
+          ? K extends string 
+            ? K | `${K}.${SimpleNestedPaths<T[K], D extends 3 ? 2 : D extends 2 ? 1 : 0> & string}`
+            : never
+          : never;
+      }[keyof T] & string;
+      
+export type PopulateParameters<Entity> = Populate<Entity, SimpleNestedPaths<Entity> >
